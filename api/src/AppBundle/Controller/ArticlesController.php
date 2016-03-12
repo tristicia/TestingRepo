6312b669c71feea52f172709bc107e6eef4e69e9
George <George>
2016-03-12 12:24:41 +1000

    Updated Article Controller

--- api/src/AppBundle/Controller/ArticlesController.php
+++ api/src/AppBundle/Controller/ArticlesController.php
@@ -17,20 +17,14 @@ class ArticlesController extends FOSRestController
      *
      * it generates so the route GET .../articles/{id}
      */
-    public function getArticleAction($id)
-    {
-        $article = new Article("title $id", "body $id");
+    public function getArticlesAction()
+      {
+          $articles = $this
+              ->getDoctrine()
+              ->getRepository('AppBundle:Article')
+              ->findAll();
   
-          $manager = $this->getDoctrine()->getManager();
-          // persist ONLY add the object to the list of object to
-          // save
-          $manager->persist($article);
-          // only flush will actually save in database, this in order
-          // to make it possible to save a lot of object in only one flush
-          // (which is a LOT faster than flushing one by one
-          $manager->flush();
-  
-          return $article;  
-    }
+          return $articles;
+      }
 
 }
